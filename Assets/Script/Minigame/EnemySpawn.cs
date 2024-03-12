a0c90edee01dfaef83205eef6673a7299ba8aca5
yeongrin <107088371+yeongrin@users.noreply.github.com>
2024-03-12 16:05:36 +1100

    Week5/Prototype
    When player clicks object twice, go back to objects.
    
    Minigame enemyspawn

--- Assets/Script/Minigame/EnemySpawn.cs
+++ Assets/Script/Minigame/EnemySpawn.cs
@@ -4,15 +4,97 @@ using UnityEngine;
 
 public class EnemySpawn : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public enum SpawnState { SPAWNING, WAITING, COUNTING};
+
+    [System.Serializable]
+    public class Wave
     {
-        
+        public string name;
+        public Transform enemy;
+        public int count;
+        public float rates;
     }
 
-    // Update is called once per frame
+    // Start is called before the first frame update
+    public Wave[] waves;
+    private int nextWave = 0;
+
+    public float timeBetweenWaves = 5f;
+    public float waveCountdown;
+
+    private float searchCountdown = 1f;
+
+    public SpawnState state = SpawnState.COUNTING;
+
+    void Start()
+    {
+        waveCountdown = timeBetweenWaves;
+    }
+
+    
     void Update()
     {
-        
+        if(state == SpawnState.WAITING) 
+        { 
+            if (!EnemyIsAlive ())
+            {
+                //Begin a new round
+            }
+            else
+            {
+
+            }
+        }
+
+        if (waveCountdown <= 0)
+        {
+            if (state != SpawnState.SPAWNING)
+            {
+                StartCoroutine(SpawnWave(waves[nextWave]));
+            }
+        }
+
+        else
+        {
+            waveCountdown -= Time.deltaTime;
+        }
+
+    }
+
+    bool EnemyIsAlive()
+    {
+        searchCountdown -= Time.deltaTime;
+
+        if (searchCountdown <= 0)
+        { 
+            if (GameObject.FindGameObjectsWithTag ("Enemy") == null)
+        {
+            return false;
+        }
+
+           
+        }
+       return true;
+    }
+
+    IEnumerator SpawnWave(Wave _waves)
+    {
+        state = SpawnState.SPAWNING;
+        //Spawn
+
+        for (int i = 0; i < _waves.count; i++)
+        {
+            SpawnEnemy(_waves.enemy);
+        }
+
+        state = SpawnState.WAITING;
+
+        yield break;
+    }
+
+    void SpawnEnemy(Transform _enemy)
+    {
+        //Spawn.enemy
+        Debug.Log("Spawning enemy!:" + _enemy.name);
     }
 }
--- Assets/Script/OperaHouse1.cs
+++ Assets/Script/OperaHouse1.cs
@@ -17,38 +17,92 @@ public class OperaHouse1 : MonoBehaviour
     public float duration;
 
     public bool test;
+    public bool test2;
+
+    [Header("Double click")]
+    public float DoubleClickSpeed = 0.25f;
+    private bool isOneClick = false;
+    private double Timer = 0;
 
     void Start()
     {
         startTime = Time.time;
+
+        startPoint.transform.position = transform.position;
     }
 
-    
+
     void Update()
     {
 
+        /*if (isOneClick && (Time.time - Timer) > DoubleClickSpeed)
+        {
+            Debug.Log("One clicked");
+            isOneClick = false;
+        }*/
+
         if (Input.GetMouseButtonDown(0))
         {
-            Vector2 pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            RaycastHit2D hit = Physics2D.Raycast(pos, Vector2.zero, 0f);
 
-            if (hit.transform.gameObject.tag == "Object2" && hit.collider != null)
+            if (!isOneClick)
             {
-                float progress = (Time.deltaTime - startTime) / duration;
-                progress = Mathf.Clamp(progress, 0, 1);
+                print("firstone");
+                Timer = Time.time;
+                isOneClick = true;
+                Vector2 pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+                RaycastHit2D hit = Physics2D.Raycast(pos, Vector2.zero, 0f);
 
-                Vector2 startPost = startPoint.transform.position;
-                Vector2 targetPost = endPoint.transform.position;
-                //Vector2 originpo = (startPost + (targetPost - startPost) * progress + new Vector2(-1f, 0) * speed);
-                //transform.position = originpo;
-                //transform.position = endPoint.transform.position;
-                test = true;
+                if (hit.transform.gameObject.tag == "Object2" && hit.collider != null)
+                {
+                    float progress = (Time.deltaTime - startTime) / duration;
+                    progress = Mathf.Clamp(progress, 0, 1);
+
+                    Vector2 startPost = startPoint.transform.position;
+                    Vector2 targetPost = endPoint.transform.position;
+                    //Vector2 originpo = (startPost + (targetPost - startPost) * progress + new Vector2(-1f, 0) * speed);
+                    //transform.position = originpo;
+                    //transform.position = endPoint.transform.position;
+                    test = true;
+                    test2 = false;
+                }
+            } else
+            {
+                print("secondone");
+                Timer = Time.time;
+                isOneClick = false;
+                Vector2 pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+                RaycastHit2D hit = Physics2D.Raycast(pos, Vector2.zero, 0f);
+
+                if (hit.transform.gameObject.tag == "Object2" && hit.collider != null)
+                {
+                    float progress = (Time.deltaTime - startTime) / duration;
+                    progress = Mathf.Clamp(progress, 0, 1);
+
+                    Vector2 startPost = startPoint.transform.position;
+                    Vector2 targetPost = endPoint.transform.position;
+                    //Vector2 originpo = (startPost + (targetPost - startPost) * progress + new Vector2(-1f, 0) * speed);
+                    //transform.position = originpo;
+                    //transform.position = endPoint.transform.position;
+                    test2 = true;
+                    test = false;  
+                }
             }
+
+        }
+       
+
+        if (test == true && test2 == false)
+        {
+            //print("jjgjgejge");
+            transform.position = Vector3.Lerp(transform.position, endPoint.transform.position, speed);
         }
 
-        if (test == true)
+        if (test2 == true && test == false)
         {
-            transform.position = Vector3.Lerp(transform.position, endPoint.transform.position, speed);
+            //print("jjgjgejge");
+            transform.position = Vector3.Lerp(transform.position, startPoint.transform.position, speed);
         }
     }
 }
+
+       
