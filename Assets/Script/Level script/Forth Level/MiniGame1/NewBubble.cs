550e28895aaa8cfc1c6c0a2ea08ed9318127b92c
Coolinglight <91443307+Coolinglight@users.noreply.github.com>
2024-07-16 15:48:24 +1000

    Adding changes from branch-1 to main
    Adding functionality for cars to move out of the way when horn is pressed in level 1 and bubble shake is fixed in level 4

--- Assets/Script/Level script/Forth Level/MiniGame1/NewBubble.cs
+++ Assets/Script/Level script/Forth Level/MiniGame1/NewBubble.cs
@@ -1,23 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class NewBubble : MonoBehaviour
 {
 
     public bool isShaking = false;
-
+    public Transform startPos;
     public float shakeAmount;
-
+    
     void Update()
     {
+
         if (isShaking)
         {
-            Vector3 newPos = Random.insideUnitSphere * (Time.deltaTime * shakeAmount);
+            /*Vector3 newPos = Random.insideUnitSphere * (Time.deltaTime * shakeAmount);
             newPos.y = transform.position.y;
-            newPos.z = transform.position.z;
+            newPos.z = transform.position.z;*/
+            //StartCoroutine(TestShake());
+            
 
-            transform.position = newPos;
+            //transform.position = newPos;
             //While isShaking is true, the position of the object will change to a new position to cause the shake effect.
         }
     }
@@ -35,8 +39,17 @@ public class NewBubble : MonoBehaviour
         if(isShaking == false)
         {
             isShaking = true;
-        } 
+        }
         //If it isn't shaking already, start shaking the object.
+
+        transform.localPosition += new Vector3(10, 0, 0);
+        yield return new WaitForSeconds(0.1f);
+        transform.localPosition -= new Vector3(10, 0, 0);
+        yield return new WaitForSeconds(0.1f);
+        transform.localPosition += new Vector3(5, 0, 0);
+        yield return new WaitForSeconds(0.1f);
+        transform.localPosition -= new Vector3(5, 0, 0);
+        yield return new WaitForSeconds(0.1f);
 
         yield return new WaitForSeconds(0.5f);
 
@@ -45,4 +58,5 @@ public class NewBubble : MonoBehaviour
         //After a set amount of time has passed, set the isShaking to false and set the position of the object back to its original position.
     }
 
+    
 }
