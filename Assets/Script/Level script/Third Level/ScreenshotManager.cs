039e8235883f77d7281528b4595c6c03e02facfc
Coolinglight <91443307+Coolinglight@users.noreply.github.com>
2024-07-19 15:14:52 +1000

    Added screenshot
    Added a function that allows clicking on the photo icon to take a screenshot for use in photo album minigame. (still need to figure out how to add the images to the mini game)  Also includes function to automatically delete images present when level 3 starts.

--- Assets/Script/Level script/Third Level/ScreenshotManager.cs
+++ Assets/Script/Level script/Third Level/ScreenshotManager.cs
@@ -1,23 +1,42 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEditor;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.UIElements;
+using Image = UnityEngine.UI.Image;
 
 public class ScreenshotManager : MonoBehaviour
 {
-    // Start is called before the first frame update
+    int maxAllowedScreenshots = 5;
+    int nextScreenshotIndex = 0;
+
+    public List<Sprite> screenshots = new List<Sprite>();
+
     void Start()
     {
-        
+        DeleteAllInFolder();
     }
 
-    // Update is called once per frame
-    void Update()
+    void DeleteAllInFolder()
     {
-        
+        string[] photoFolder = { "Assets/Screenshots" };
+        foreach(var photo in AssetDatabase.FindAssets("", photoFolder))
+        {
+            var path = AssetDatabase.GUIDToAssetPath(photo);
+            AssetDatabase.DeleteAsset(path);
+        }
     }
 
     public void TakeScreenshot()
     {
-        ScreenCapture.CaptureScreenshot(Application.dataPath + "/Screenshots/" + "Level3Screenshot.png");
+        if (nextScreenshotIndex < maxAllowedScreenshots)
+        {
+            ScreenCapture.CaptureScreenshot(Application.dataPath + "/Screenshots/" + "Level3Screenshot" + nextScreenshotIndex + ".png");
+            nextScreenshotIndex++;
+        }
+        else
+            return;
     }
 }
